0e13105ed62028d374a849feb8eeeb5c05f68461
smazik02 <s.mazik2002@gmail.com>
2024-03-19 22:42:26 +0100

    servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+#define SERVO 23
 
 // put function declarations here:
-int myFunction(int, int);
+Servo myServo;
+int pos = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // put your setup code here, to run once:
+    myServo.attach(SERVO);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    for (pos = 0; pos <= 180; pos++) {
+        myServo.write(pos);
+        delay(15);
+    }
+    for (pos = 180; pos >= 0; pos--) {
+        myServo.write(pos);
+        delay(15);
+    }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
