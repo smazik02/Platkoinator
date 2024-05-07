aa5547346ba272b53fae974cb573ce0a13dfdea2
smazik02 <s.mazik2002@gmail.com>
2024-05-07 22:34:32 +0200

    Set ports

--- include/constants.h
+++ include/constants.h
@@ -2,29 +2,29 @@
 #define CONSTANTS
 
 // Change pins
-#define BELT 1
-#define BELT_FORWARD 1
-#define BELT_BACK 1
-#define LED 1
-#define PUMP 1
-#define PUMP_FORWARD 1
-#define PUMP_BACK 1
-#define SENSOR_START 4
-#define SENSOR_MILK 5
-#define SENSOR_CEREAL_1 1
-#define SENSOR_CEREAL_2 2
-#define SENSOR_CEREAL_3 3
-#define SENSOR_END 4
-#define SERVO_CEREAL_1 10
-#define SERVO_CEREAL_2 11
-#define SERVO_CEREAL_3 12
-#define SOLENOID_1 1
-#define SOLENOID_2 2
+#define BELT 22
+#define BELT_FORWARD 19
+#define BELT_BACK 29
+#define LED 14
+#define PUMP 17
+#define PUMP_FORWARD 0
+#define PUMP_BACK 16
+#define SENSOR_START 36
+#define SENSOR_MILK 39
+#define SENSOR_CEREAL_1 34
+#define SENSOR_CEREAL_2 35
+#define SENSOR_CEREAL_3 32
+#define SENSOR_END 33
+#define SERVO_CEREAL_1 25
+#define SERVO_CEREAL_2 26
+#define SERVO_CEREAL_3 27
+#define SOLENOID_1 12
+#define SOLENOID_2 13
 
 // Adjust sensor sensitivity and engine speeds
-#define BELT_SPEED 100
-#define PUMP_SPEED 100
-#define SENSOR_SENSITIVITY 100
+#define BELT_SPEED 5
+#define PUMP_SPEED 255
+#define SENSOR_SENSITIVITY 30
 
 // LEDs
 #define LED_COUNT 20
